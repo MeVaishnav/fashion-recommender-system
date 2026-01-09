0562c9f5b4abe8001f4e7eddaa81619d27f63d89
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-01-09 23:26:28 +0530

    normalize the result

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.utils import load_img, img_to_array
 from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
 import numpy as np
+from numpy.linalg import norm
 
 model=ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
 model.trainable=False
@@ -21,3 +22,4 @@ def extract_features(img_path,model):
     expanded_img_array=np.expand_dims(img_array,axis=0)
     preprocessed_img=preprocess_input(expanded_img_array)
     result=model.predict(preprocessed_img).flatten()
+    nomalized_result=result/norm(result)
